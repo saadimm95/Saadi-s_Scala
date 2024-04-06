f2a59b8c60aec9e0cae58c7234624c24bae1e374
saadimm95 <saadimm95@gmail.com>
2024-04-06 20:30:58 +0530

    first commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,70 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+//    println("Hello world!")
+    // prime Number Program
+//    var count=0;
+//    var a= 10;
+//
+//    for (i<- 1 to 21) {
+//      if (a%i== 0)
+//    {
+//      count= count+1;
+//    }
+//      }
+//    if (count == 2 )
+//      {
+//        println("Number is prime")
+//      }
+//    else {
+//      println("number is non prime")
+//    }
+//  }
+
+//    var a: Int= 100
+//
+//    if((a >= 100)&&(a<= 1000))
+//    {
+//      if(a%2 == 0)
+//      {
+//        print("Modulo with 3 " +a%3)
+//      }
+//      else
+//      {
+//        print("Modulo with 2 " +a%2)
+//      }
+//    }
+//    else
+//    {
+//      print("WRONG NUMBER!!!")
+//    }
+//
+//
+//
+//  }
+//    Parten Printing
+//    var count = 0;
+//for (i<-1 to 5)
+//  {
+//  for (j<-1 to i) {
+//    print(i +" ")
+//
+//    count  = i + 1;
+//  }
+//    println()
+//  }
+//  }
+//}
+
+// Reverse Parten Printing
+ var num = 0
+    for(i<-1 to 5)
+      {
+      for(j<-5 to 1 by -5)
+        {
+          println(i +" ")
+        }
+        num = i+1;
+      }
+      println()
+         }
+  }
